ffe5265ac8da96ce467514b08aca0ad757ceb365
lauri <lauri.paloheimo@live.com>
2025-01-14 20:07:38 +0200

    First commit

---
+++ Moduuli 2/02-Teht6-LockCode.py
@@ -0,0 +1,9 @@
+import random
+
+
+
+
+LukkoKoodi1 = (random.sample(range(0,9), 3))
+LukkoKoodi2 = (random.sample(range(1,6), 4))
+
+print("Lukon Yksi koodi on =",LukkoKoodi1,"\nLukon kaksi koodi on = ", LukkoKoodi2)
