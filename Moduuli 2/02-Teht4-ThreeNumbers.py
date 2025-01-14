78e5e1404501ed317c2b774bf41434853e7c8513
lauri <lauri.paloheimo@live.com>
2025-01-14 15:58:33 +0200

    First commit

---
+++ Moduuli 2/02-Teht4-ThreeNumbers.py
@@ -0,0 +1,11 @@
+Num1 = int(input("Anna numero 1 = "))
+Num2 = int(input("Anna numero 2 = "))
+Num3 = int(input("Anna numero 3 = "))
+
+Summa = Num1 + Num2 + Num3
+
+Tulo = Num1 * Num2 * Num3
+
+
+
+print("Numeroiden summa on = ", Summa , "ja Tulo on = ", Tulo)
