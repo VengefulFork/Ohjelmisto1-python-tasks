8f75be04e89e416c3cf0bea6e8dfc4ec9d283f5f
lauri <lauri.paloheimo@live.com>
2025-01-14 15:52:56 +0200

    First commit

---
+++ Moduuli 2/02-Teht3-Rectangle.py
@@ -0,0 +1,11 @@
+import math
+
+Kanta = float(input("Anna suorakulmion kanta = "))
+
+Korkeus = float(input("Anna suorakulmion korkeus = "))
+
+PintaAla = Kanta * Korkeus
+
+Piiri = (Kanta * 2) + (Korkeus * 2)
+
+print("Suorakulmion pinta-ala on ", PintaAla, "ja Piiri on ", Piiri)
