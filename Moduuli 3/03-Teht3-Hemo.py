062852b94e8fff2663936ec6ba14f72903faca3a
lauri <lauri.paloheimo@live.com>
2025-01-15 12:29:13 +0200

    First commit

--- Moduuli 3/03-Teht3-Hemo.py
+++ Moduuli 3/03-Teht3-Hemo.py
@@ -6,8 +6,14 @@ Sukupuoli = Sukupuoli.lower()
 if Sukupuoli == "nainen" and 117<=Hemo<=175:
     print("Normaali Hemoglobiini arvo naiselle")
 elif Sukupuoli == "nainen" and Hemo<=116:
-    print("test")
+    print("Alhainen Hemoglobiini arvo naiselle")
 elif Sukupuoli == "nainen" and Hemo>=176:
-    print("test2")
+    print("Korkea Hemoglobiini arvo naiselle")
 elif Sukupuoli == "mies" and 134<=Hemo<=195:
-    print("test3")
+    print("Normaali Hemoglobiini arvo miehelle")
+elif Sukupuoli == "mies" and Hemo<=134:
+    print("Alhainen Hemoglobiini arvo miehelle")
+elif Sukupuoli == "mies" and Hemo>=196:
+    print("Korkea Hemoglobiini arvo miehelle")
+else :
+    print ("Ei hyväksyttyjä arvoja syötetty kokeile uudestaan")
