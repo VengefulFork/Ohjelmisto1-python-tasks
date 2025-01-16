247feeedf78180edca2cbeadfa3ac70f4f4c14b6
lauri <lauri.paloheimo@live.com>
2025-01-16 13:05:34 +0200

    First Commit

--- Moduuli 2/02-Teht5-UnitConver.py
+++ Moduuli 2/02-Teht5-UnitConver.py
@@ -5,13 +5,13 @@ Leiviskät = float(input("Anna leivisköiden määrä = "))
 Naulat = float(input("Anna naulojen määrä = "))
 
 Luodit = float(input("Anna luotien määrä = "))
-
+#Muunnetaan Leiviskät Nauloiksi
 Muunnos1 = (Leiviskät * 20)
-
+#Lisätään muunnetut leiviskät annettuihin nauloihin ja muunnetaan ne luodeiksi
 Muunnos2 = (Naulat + Muunnos1) * 32
-
+#Lisätään muunnetut luoteihin ja muutetaan  ne grammoiksi
 Muunnos3 = float((Muunnos2 + Luodit) * 13.3)
-
+#Muunnokset Kiloiksi ja grammoiksi
 Kilot = int(Muunnos3 / 1000)
 
 Grammat = Muunnos3 % 1000
--- Moduuli 2/02-Teht6-LockCode.py
+++ Moduuli 2/02-Teht6-LockCode.py
@@ -3,7 +3,7 @@ import random
 
 
 
-LukkoKoodi1 = (random.sample(range(0,9), 3))
-LukkoKoodi2 = (random.sample(range(1,6), 4))
+LukkoKoodi1 = (random.sample(range(0,10), 3))
+LukkoKoodi2 = (random.sample(range(1,7), 4))
 
 print("Lukon Yksi koodi on =",LukkoKoodi1,"\nLukon kaksi koodi on = ", LukkoKoodi2)
