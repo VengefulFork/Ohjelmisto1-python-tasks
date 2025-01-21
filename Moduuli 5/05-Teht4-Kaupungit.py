e8731dd5ff85be6d07030e9617d99e3ecc10cf03
lauri <lauri.paloheimo@live.com>
2025-01-21 13:34:47 +0200

    Valmis

--- Moduuli 5/05-Teht4-Kaupungit.py
+++ Moduuli 5/05-Teht4-Kaupungit.py
@@ -4,5 +4,10 @@ for i in range(5):
     Kaupungit = input("Anna kaupungin nimi = ")
     KaupungitLista.append(Kaupungit)
 
-for i in KaupungitLista[:5]:
-    print(i)
+
+for KaupungitLista in KaupungitLista:
+    print(KaupungitLista)
+
+# Tämäkin toimii
+# for i in KaupungitLista[:5]:
+#     print(i)
