6732d19b50efd6e9787ce7d188c6fffd92b3bedb
lauri <lauri.paloheimo@live.com>
2025-01-21 13:30:49 +0200

    Valmis

---
+++ Moduuli 5/05-Teht3-AlkuLuku.py
@@ -0,0 +1,18 @@
+import math
+Lippu = True
+Luku = int(input("Anna luku = "))
+
+
+
+
+if Luku > 1:
+
+        for i in range(2, Luku):
+                if (Luku % i ) == 0:
+                        Lippu = False
+
+
+if Lippu:
+        print("On alkuluku")
+else :
+        print("Ei ole alkuluku")
---
+++ Moduuli 5/05-Teht4-Kaupungit.py
@@ -0,0 +1,8 @@
+KaupungitLista = []
+
+for i in range(5):
+    Kaupungit = input("Anna kaupungin nimi = ")
+    KaupungitLista.append(Kaupungit)
+
+for i in KaupungitLista[:5]:
+    print(i)
