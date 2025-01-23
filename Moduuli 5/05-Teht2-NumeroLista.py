6e3cadf226b60be3e826e7340ca52db2fd1a5527
lauri <lauri.paloheimo@live.com>
2025-01-23 10:23:27 +0200

    Switched from converting string list to just appending as float to empty list

--- Moduuli 5/05-Teht2-NumeroLista.py
+++ Moduuli 5/05-Teht2-NumeroLista.py
@@ -8,21 +8,20 @@ while loop :
     numero = input("Anna numero jatkaaksesi tai tyhjä lopetaaksesi = ")
     try:
         if float(numero):
-            lista.append(numero)
+            lista.append(float(numero))
 
             continue
     except:
         if numero == "":
             loop = False
 
-numero_lista = [float(i) for i in lista]
 
-numero_lista.sort(reverse=True)
+lista.sort(reverse=True)
 # Lista.sort()
 #Metodi Yksi
-print(numero_lista[:5])
+print(lista[:5])
 #Metodi Kaksi
-for i in numero_lista[:5]:
+for i in lista[:5]:
     print(i)
 
 # print(Lista)
