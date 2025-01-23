1aba1170a7d7ef0aaa4c077f01e2810a000b7fae
lauri <lauri.paloheimo@live.com>
2025-01-23 10:05:09 +0200

    Added the intended solving method of while loop

--- Moduuli 4/04-Teht1-Kolme.py
+++ Moduuli 4/04-Teht1-Kolme.py
@@ -1,5 +1,24 @@
-while True:
-    for i in range(1,1001):
-        if i % 3 == 0:
-            print(i)
-    break
+# Toinen tapa ratkaista for loop:ia käyttäen
+# for i in range(1,1001):
+#      if i % 3 == 0:
+#         print(i)
+
+toistot = 1
+
+while toistot <= 1000:
+
+    if toistot % 3 == 0:
+        print(toistot)
+
+    toistot += 1
+
+
+
+
+
+
+
+
+
+
+
