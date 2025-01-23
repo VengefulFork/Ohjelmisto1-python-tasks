a496bdf4125b6af9d2cca51fbfe5d86a29e14cbe
lauri <lauri.paloheimo@live.com>
2025-01-23 11:16:52 +0200

    Added a solution using no list

--- Moduuli 4/04-Teht3-Lista.py
+++ Moduuli 4/04-Teht3-Lista.py
@@ -1,27 +1,55 @@
 import math
 
-loop = "o"
+loop = True
 
-numero_lista = []
+suurin_luku = float()
+pienin_luku = float()
 
-while loop != "":
-    numero = input("Anna numero jatkaaksesi tai tyhjä lopetaaksesi = ")
-    try:
-        if float(numero):
-            numero_lista.append(numero)
+toisto_yksi = True
 
-            continue
+while loop :
+    annettu_luku = input("Anna jokin luku tai tyhjä pysäyttääksesi= ")
+
+    try :
+        annettu_luku = float(annettu_luku)
+        if toisto_yksi :
+            suurin_luku = pienin_luku = annettu_luku
+            toisto_yksi = False
+        else :
+            if annettu_luku < pienin_luku :
+                pienin_luku = annettu_luku
+            elif annettu_luku > suurin_luku:
+                suurin_luku = annettu_luku
+
     except:
-        if numero == "":
-            loop = numero
-
-float_list = [float(i) for i in numero_lista]
-
-float_list.sort()
-
-print("Pienin luku on", float_list[0], "Suurin luku on", float_list[-1])
+        if annettu_luku == "":
+            loop = False
+        else :
+            print("Ei hyväksytty input")
 
 
+print ("Suurin luku on",suurin_luku, "Pienin luku on",  pienin_luku)
+
+
+# Listaa käyttäen toisenlainen ratkaisu
+
+# loop = "o"
+#
+# numero_lista = []
+#
+# while loop != "":
+#     numero = input("Anna numero jatkaaksesi tai tyhjä lopetaaksesi = ")
+#     try:
+#         if float(numero):
+#             numero_lista.append(float(numero))
+#
+#             continue
+#     except:
+#         if numero == "":
+#             loop = numero
+#
+# numero_lista.sort()
+# print("Pienin luku on", numero_lista[0], "Suurin luku on", numero_lista[-1])
 
 
 
