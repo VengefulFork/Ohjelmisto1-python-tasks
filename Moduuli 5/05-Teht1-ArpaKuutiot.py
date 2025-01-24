99e036a0f6d97b6682a2e9ed624691c736d27ec6
lauri <lauri.paloheimo@live.com>
2025-01-24 11:09:00 +0200

    Added the sum part

--- Moduuli 5/05-Teht1-ArpaKuutiot.py
+++ Moduuli 5/05-Teht1-ArpaKuutiot.py
@@ -1,9 +1,15 @@
 import math
 import random
 
+summa = []
 
 noppa_määrä = int(input("Anna noppien määrä numeroin = "))
 
 for i in range(noppa_määrä):
-    print(random.randint(1,6))
+    summa.append(int(random.randint(1,6)))
+    listan_summa = sum(summa)
+
+print(summa)
+print(listan_summa)
+
 
