1bb36b3a6ec087bfe07783c7d0af1e7ca99331bb
lauri <lauri.paloheimo@live.com>
2025-01-28 13:20:26 +0200

    Valmis?

---
+++ Moduuli 6/06-Teht5-KarsittuLista.py
@@ -0,0 +1,19 @@
+import math
+import random
+
+# Luodaan lista kokonaislukuja
+alkuperäinen_lista = random.sample(range(1, 101), random.randint(3, 7))
+
+# Funktio toisen listan luomiseen joka ei sisällä parittomia lukuja
+def karsija(alkuperäinen_lista):
+    karsittu_lista = []
+    # Testataanko onko luku tasan jaollinen kahdella ja jos on lisätään uuteen listaan.
+    for i in alkuperäinen_lista:
+        if i % 2 == 0:
+            karsittu_lista.append(int(i))
+
+
+    return karsittu_lista
+
+print("Alkuperäinen kaikki luvut sisältävä lista",alkuperäinen_lista)
+print("Ei parittomia lukuja sisältävä list",karsija(alkuperäinen_lista))
