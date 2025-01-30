427f20c3c7b32d736fcf2a182be297981804c4cd
lauri <lauri.paloheimo@live.com>
2025-01-30 09:08:45 +0200

    Comments and renamed variables

--- Moduuli 5/05-Teht1-ArpaKuutiot.py
+++ Moduuli 5/05-Teht1-ArpaKuutiot.py
@@ -1,15 +1,16 @@
 import math
 import random
 
-summa = []
+noppien_silmäluvut = []
 
 noppa_määrä = int(input("Anna noppien määrä numeroin = "))
 
 for i in range(noppa_määrä):
-    summa.append(int(random.randint(1,6)))
-    listan_summa = sum(summa)
-
-print(summa)
-print(listan_summa)
+    noppien_silmäluvut.append(int(random.randint(1, 6)))
+    listan_summa = sum(noppien_silmäluvut)
+# Tulostetaan kaikkien heitettyjen noppien silmäluvut, lähinnä testausta varten
+print("Heitettyjen noppien silmäluvut ovat",noppien_silmäluvut)
+# Tulostestaan noppien silmälukujen summa
+print("Kaikkien heitettyjen noppien silmälukujen summa on",listan_summa)
 
 
