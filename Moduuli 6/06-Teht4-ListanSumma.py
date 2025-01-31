e6b27e9a10aee505c78197a0c73fc6b2668ac7d6
lauri <lauri.paloheimo@live.com>
2025-01-31 20:53:13 +0200

    Moved code around

--- Moduuli 6/06-Teht4-ListanSumma.py
+++ Moduuli 6/06-Teht4-ListanSumma.py
@@ -1,8 +1,7 @@
 import math
 import random
 
-# Numero listan luominen
-numero_lista = random.sample(range(1,101),random.randint(3,6))
+
 
 # Funktio listan numeroiden summan laskemiseen
 def summa_funktio(numero_lista):
@@ -12,6 +11,8 @@ def summa_funktio(numero_lista):
         listan_summa += i
     return listan_summa
 
+# Numero listan luominen
+numero_lista = random.sample(range(1,101),random.randint(3,6))
 
 # Printataan koko lista, jotta voidaan tarkistaa onko annettu summa oikea
 print("Lista numeroita",numero_lista)
