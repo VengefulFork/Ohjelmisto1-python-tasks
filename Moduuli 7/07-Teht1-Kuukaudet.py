a08d7ab7dda0b08020860215ff780535fb12b795
lauri <lauri.paloheimo@live.com>
2025-02-04 13:37:06 +0200

    Fixed thingies

--- Moduuli 7/07-Teht1-Kuukaudet.py
+++ Moduuli 7/07-Teht1-Kuukaudet.py
@@ -4,13 +4,13 @@ vuoden_ajat = ("kevät", "kesä", "syksy", "talvi")
 
 kuukauden_numero = int(input("Anna kuukaden numero kokonais lukuna = "))
 
-if  kuukauden_numero == 3 or 4 or 5 :
+if  3<= kuukauden_numero <=5 :
     print(vuoden_ajat[0])
-if kuukauden_numero == 6 or 7 or 8:
+elif 6<= kuukauden_numero <=8:
     print(vuoden_ajat[1])
-if kuukauden_numero == 9 or 10 or 11:
+elif 9<= kuukauden_numero <=11:
     print(vuoden_ajat[2])
-if kuukauden_numero == 12 or 1 or 2 :
+elif kuukauden_numero == 12 or 1 or 2 :
     print(vuoden_ajat[3])
 else :
     print("Luku on liian suuri tai pieni")
