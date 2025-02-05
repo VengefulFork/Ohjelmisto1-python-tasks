c246537c5b8615515011817de4da061f0d51b9c1
lauri <lauri.paloheimo@live.com>
2025-02-05 11:25:09 +0200

    Fixed input to always be uppercase

--- Moduuli 8/08-Teht1-SqlHaku.py
+++ Moduuli 8/08-Teht1-SqlHaku.py
@@ -1,12 +1,12 @@
 import mariadb
 
-def sqlkomento (icao):
+def haku (icao):
     sql = f"SELECT name, municipality FROM airport where ident = '{icao}'"
     curs = yhteys.cursor()
     curs.execute(sql)
     tulos = curs.fetchall()
-    if curs.rowcount >0 :
-        for rivi in tulos:
+    # if curs.rowcount >0 :
+    for rivi in tulos:
             print(f"ICAO koodia vastaava kenttä on {rivi[0]} ja sijantikunta {rivi[1]}")
     return
 
@@ -22,6 +22,6 @@ yhteys = mariadb.connect(
 
 icao = input("anna icao koodi = ").upper()
 
-sqlkomento(icao)
+haku(icao)
 
 
