0bfa5dbe62e10210311389c17f30ad7e105c6093
lauri <lauri.paloheimo@live.com>
2025-02-06 10:03:29 +0200

    Math proudly stolen from stackoverflow rest is mine

---
+++ Moduuli 8/08-teht3-Koordinaatit-Ilman-Geopy.py
@@ -0,0 +1,49 @@
+import mariadb
+from math import sin, cos, sqrt, atan2, radians
+
+yhteys = mariadb.connect(
+    host='localhost',
+    port=3306,
+    database='flight_game',
+    user='flightuser',
+    password='1234',
+    autocommit=True
+)
+
+def koordinaatit(icao):
+    sql = f"SELECT latitude_deg, longitude_deg FROM airport WHERE ident = '{icao}'"
+    curs = yhteys.cursor()
+    curs.execute(sql)
+    tulos = curs.fetchall()
+
+    for rivi in tulos:
+        lat = rivi[0]
+        long = rivi[1]
+
+    return lat, long
+
+icao = input("Syotä kentän yksi ICAO koodi = ").upper()
+
+lat1 = radians(koordinaatit(icao)[0])
+long1 = radians(koordinaatit(icao)[1])
+
+icao = input("Syotä kentän kaksi ICAO koodi = ").upper()
+
+lat2 = radians(koordinaatit(icao)[0])
+long2 = radians(koordinaatit(icao)[1])
+
+# print(lat1, long1)
+# print(lat2, long2)
+
+# Maan säde
+r = 6378.1
+
+lat_laskuun = lat2 - lat1
+long_laskuun = long2 - long1
+
+a = (sin(lat_laskuun/2))**2 + cos(lat1) * cos(lat2) * (sin(long_laskuun/2)) ** 2
+c = 2 * atan2(sqrt(a), sqrt(1-a))
+
+etäisyys = r * c
+
+print(f"Kentien välinen etäisyys on {round(etäisyys, 3)} kilometriä")
